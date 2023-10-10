d3490d274d30eef707a8fe5c4d38602c19e13091
Rafael Najarro <rafaelnajarro10@gmail.com>
2023-10-10 17:31:50 -0600

    guia 08

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import Formulario from './src/components/Formulario';
+import Pais from './src/components/Pais';
 
 export default function App() {
+  const [busqueda, guardarbusqueda] = useState({
+    pais: '',
+  });
+  const [consultar, guardarconsultar] = useState(false);
+  const [resultado, guardarresultado] = useState({});
+  const [resultado2, guardarresultado2] = useState({});
+  useEffect(() => {
+    const { pais } = busqueda;
+    const consultarPais = async () => {
+      if (consultar) {
+        const url =
+          `https://servicodados.ibge.gov.br/api/v1/paises/${pais}`;
+        const urlBandera =
+          `https://flagcdn.com/w20/${pais}.png`;
+        console.log(url)
+        console.log(urlBandera)
+        try {
+          const respuesta = await fetch(url);
+          const respuesta2 = await fetch(urlBandera);
+          const resultado = await respuesta.json();
+          const resultado2 = await respuesta2.json();
+          guardarresultado(resultado);
+          guardarresultado2(resultado2);
+          guardarconsultar(false);
+        } catch (error) {
+          mostrarAlerta();
+        }
+      }
+    };
+    consultarPais();
+  }, [consultar]);
+  const mostrarAlerta = () => {
+    Alert.alert('Error', 'No hay resultado intenta con otra ciudad o país'),
+      [{ Text: 'Ok' }];
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.app}>
+      <View style={styles.contenido}>
+        <Formulario
+          busqueda={busqueda}
+          guardarbusqueda={guardarbusqueda}
+          guardarconsultar={guardarconsultar}
+        />
+        <Pais resultado={resultado} resultado2={resultado2}/>
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'rgb(71,149,212)',
     justifyContent: 'center',
   },
+  contenido: {
+    margin: '2.5%',
+  },
 });
+
+
---
+++ src/components/Pais.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { Card } from 'react-native-elements';
+
+const Pais = ({ resultado, resultado2 }) => {
+    const [info, setinfo] = useState([]);
+    const [info2, setinfo2] = useState([]);
+    const [nombre, setnombre] = useState();
+    const [capital, setcapital] = useState();
+    const [region, setregion] = useState();
+    const [lengua, setlengua] = useState([]);
+    const [area, setArea] = useState([]);
+    const [bandera, setBandera] = useState([]);
+
+    useEffect(() => {
+        setinfo(resultado);
+        setinfo2(resultado2);
+        lengua.length = 0;
+        Object.values(info).map(e => {
+            setnombre(e.nome.abreviado);
+            setcapital(e.governo.capital.nome);
+            setregion(e.localizacao.regiao.nome); Object.values(e.linguas).map(l => {
+                lengua.push(l.nome)
+            })
+            setArea(e.area.total + ' ' + e.area.unidade.símbolo)
+        }
+        );
+
+        Object.values(info2).map(e => {
+            console.log(e)
+        }
+        );
+    });
+    return (
+        <Card>
+            <Card.Title>{nombre}</Card.Title>
+            <Card.Divider />
+            <View style={{ justifyContent: 'center' }}>
+                <Text style={styles.text}>Capital: {capital}</Text>
+                <Text style={styles.text}>Region: {region}</Text>
+                <Text style={styles.text}>Lengua: {lengua.toString()}</Text>
+                <Text style={styles.text}>Área: {area}</Text>
+            </View>
+        </Card>
+    );
+};
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 22,
+    },
+    bandera: {
+        width: '100%'
+    }
+});
+export default Pais;
